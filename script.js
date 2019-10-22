4d3703e1b085eb9b051f13ef61ab8ea9d9318ce1
NickMckenney <mckenney2001@gmail.com>
2019-10-22 17:42:06 -0400

    DID THE FIRST ONEgit add .git add .git add .git add .

--- script.js
+++ script.js
@@ -14,35 +14,38 @@ let blockInPlay = [];
 arr1.push(bottomBlock);
 arr1.push(midBlock);
 arr1.push(topBlock);
+let x = leftB.lastElementChild.innerText;
+console.log(x);
 
-// topBlock.addEventListener("click", function(evt) {
-//   topBlock.style.backgroundColor = "pink";
-//   blockSetter = topBlock;
-//   console.log("T");
-// });
-//
-// midBlock.addEventListener("click", function(evt) {
-//   midBlock.style.backgroundColor = "pink";
-//   blockSetter = midBlock;
-//   console.log("MiddleBlock");
-// });
-// bottomBlock.addEventListener("click", function(evt) {
-//   bottomBlock.style.backgroundColor = "pink";
-//   blockSetter = bottomBlock;
-// });
-
-// ELEMENTS OF CONTAINERS
 leftB.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  if (blockInPlay.length !== 0 || leftB.childElementCount !== 0) {
-    if (blockInPlay.length == 0) {
-      blockInPlay.push(leftB.lastElementChild);
-      leftB.removeChild(leftB.lastElementChild);
-    } else {
-      leftB.insertBefore(blockInPlay[0], leftB.lastchildElement);
-      blockInPlay.pop();
+  if (blockInPlay.length == 0) {
+    console.log("1");
+    evt.preventDefault();
+    if (blockInPlay.length !== 0 || leftB.childElementCount !== 0) {
+      console.log("2");
+      if (blockInPlay.length == 0) {
+        blockInPlay.push(leftB.lastElementChild);
+        leftB.removeChild(leftB.lastElementChild);
+      } else {
+        leftB.insertBefore(blockInPlay[0], leftB.lastchildElement);
+        blockInPlay.pop();
+      }
+      move += 1;
     }
-    move += 1;
+  } else if (blockInPlay[0].innerText > leftB.lastElementChild.innerText) {
+    console.log("1");
+    evt.preventDefault();
+    if (blockInPlay.length !== 0 || leftB.childElementCount !== 0) {
+      console.log("2");
+      if (blockInPlay.length == 0) {
+        blockInPlay.push(leftB.lastElementChild);
+        leftB.removeChild(leftB.lastElementChild);
+      } else {
+        leftB.insertBefore(blockInPlay[0], leftB.lastchildElement);
+        blockInPlay.pop();
+      }
+      move += 1;
+    }
   }
 });
 
@@ -50,5 +53,28 @@ mid.addEventListener("click", function(evt) {
   evt.preventDefault();
   if (blockInPlay.length !== 0 || mid.childElementCount !== 0) {
     if (blockInPlay.length == 0) {
-  
+      blockInPlay.push(mid.lastElementChild);
+      mid.removeChild(mid.lastElementChild);
+    } else {
+      console.log("TEST");
+      mid.insertBefore(blockInPlay[0], mid.lastchildElement);
+      blockInPlay.pop();
+    }
+    move += 1;
+  }
+  // mid.appendChild(blockSetter);
+});
+rightB.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  if (blockInPlay.length !== 0 || rightB.childElementCount !== 0) {
+    if (blockInPlay.length == 0) {
+      blockInPlay.push(rightB.lastElementChild);
+      rightB.removeChild(rightB.lastElementChild);
+    } else {
+      rightB.insertBefore(blockInPlay[0], rightB.lastchildElement);
+      blockInPlay.pop();
+    }
+    move += 1;
+  }
+});
 //
