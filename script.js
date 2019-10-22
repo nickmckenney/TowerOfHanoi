46bc9f28b8047647e4757c0a7c66efc390996667
NickMckenney <mckenney2001@gmail.com>
2019-10-22 18:07:50 -0400

    GAME IS DONE.NOW FOR STYLE

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let x = leftB.lastElementChild.innerText;
 console.log(x);
 
 leftB.addEventListener("click", function(evt) {
-  if (blockInPlay.length == 0) {
+  if (blockInPlay.length == 0 || leftB.childElementCount == 0) {
     console.log("1");
     evt.preventDefault();
     if (blockInPlay.length !== 0 || leftB.childElementCount !== 0) {
@@ -50,33 +50,65 @@ leftB.addEventListener("click", function(evt) {
 });
 
 mid.addEventListener("click", function(evt) {
-  evt.preventDefault();
-
-  if (blockInPlay.length !== 0 || mid.childElementCount !== 0) {
-    if (blockInPlay.length == 0) {
-      blockInPlay.push(mid.lastElementChild);
-      mid.removeChild(mid.lastElementChild);
-    } else {
-      console.log("TEST");
-      mid.insertBefore(blockInPlay[0], mid.lastchildElement);
-      blockInPlay.pop();
+  if (blockInPlay.length == 0 || mid.childElementCount == 0) {
+    console.log("1");
+    evt.preventDefault();
+    if (blockInPlay.length !== 0 || mid.childElementCount !== 0) {
+      console.log("2");
+      if (blockInPlay.length == 0) {
+        blockInPlay.push(mid.lastElementChild);
+        mid.removeChild(mid.lastElementChild);
+      } else {
+        mid.insertBefore(blockInPlay[0], mid.lastchildElement);
+        blockInPlay.pop();
+      }
+      move += 1;
     }
-    move += 1;
+  } else if (blockInPlay[0].innerText > mid.lastElementChild.innerText) {
+    console.log("1");
+    evt.preventDefault();
+    if (blockInPlay.length !== 0 || mid.childElementCount !== 0) {
+      console.log("2");
+      if (blockInPlay.length == 0) {
+        blockInPlay.push(mid.lastElementChild);
+        mid.removeChild(mid.lastElementChild);
+      } else {
+        mid.insertBefore(blockInPlay[0], mid.lastchildElement);
+        blockInPlay.pop();
+      }
+      move += 1;
+    }
   }
-
-  // mid.appendChild(blockSetter);
 });
+
 rightB.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  if (blockInPlay.length !== 0 || rightB.childElementCount !== 0) {
-    if (blockInPlay.length == 0) {
-      blockInPlay.push(rightB.lastElementChild);
-      rightB.removeChild(rightB.lastElementChild);
-    } else {
-      rightB.insertBefore(blockInPlay[0], rightB.lastchildElement);
-      blockInPlay.pop();
+  if (blockInPlay.length == 0 || rightB.childElementCount == 0) {
+    console.log("1");
+    evt.preventDefault();
+    if (blockInPlay.length !== 0 || rightB.childElementCount !== 0) {
+      console.log("2");
+      if (blockInPlay.length == 0) {
+        blockInPlay.push(rightB.lastElementChild);
+        rightB.removeChild(rightB.lastElementChild);
+      } else {
+        rightB.insertBefore(blockInPlay[0], rightB.lastchildElement);
+        blockInPlay.pop();
+      }
+      move += 1;
     }
-    move += 1;
+  } else if (blockInPlay[0].innerText > rightB.lastElementChild.innerText) {
+    console.log("1");
+    evt.preventDefault();
+    if (blockInPlay.length !== 0 || rightB.childElementCount !== 0) {
+      console.log("2");
+      if (blockInPlay.length == 0) {
+        blockInPlay.push(rightB.lastElementChild);
+        rightB.removeChild(rightB.lastElementChild);
+      } else {
+        rightB.insertBefore(blockInPlay[0], rightB.lastchildElement);
+        blockInPlay.pop();
+      }
+      move += 1;
+    }
   }
 });
-//
