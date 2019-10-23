c17f704a7f444dc80d4a9621f318d9a53fe4b3d9
NickMckenney <mckenney2001@gmail.com>
2019-10-23 11:07:39 -0400

    IDEA

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ let blockSetter;
 
 let topBlock = document.querySelector(".topCenterB");
 let midBlock = document.querySelector(".midCenterB");
+let bigBlock = document.querySelector(".bigBlock");
+let biggerBlock = document.querySelector(".biggerBlock");
+let bigBodBlock = document.querySelector(".bigBodBlock");
 let bottomBlock = document.querySelector(".bottomCenterB");
 const leftB = document.querySelector(".leftCenterBlock");
 const mid = document.querySelector(".middle");
 const rightB = document.querySelector(".rightCenterBlock");
-let move = 0;
+let h3 = document.querySelector("h3");
+let center = document.querySelector(".centerBlock");
+let h4 = document.querySelector("h4");
+let levelUp = document.querySelector(".levelUp");
+let moves = 0;
+let n = 3;
 let arr1 = [];
 let arr2 = [];
 let arr3 = [];
@@ -14,47 +22,77 @@ let blockInPlay = [];
 arr1.push(bottomBlock);
 arr1.push(midBlock);
 arr1.push(topBlock);
+levelUp.addEventListener("click", function(evt) {
+  n += 1;
+  if (n == 4) {
+    levelFour();
+  }
+  if (n == 5) {
+    levelFive();
+  }
+  if (n == 6) {
+    levelSix();
+  }
+});
 let x = leftB.lastElementChild.innerText;
-console.log(x);
 
 leftB.addEventListener("click", function(evt) {
   if (blockInPlay.length == 0 || leftB.childElementCount == 0) {
-    console.log("1");
     evt.preventDefault();
+    console.log("1");
+
     if (blockInPlay.length !== 0 || leftB.childElementCount !== 0) {
       console.log("2");
       if (blockInPlay.length == 0) {
+        console.log("3");
         blockInPlay.push(leftB.lastElementChild);
         leftB.removeChild(leftB.lastElementChild);
       } else {
+        console.log("4");
         leftB.insertBefore(blockInPlay[0], leftB.lastchildElement);
         blockInPlay.pop();
       }
-      move += 1;
     }
   } else if (blockInPlay[0].innerText > leftB.lastElementChild.innerText) {
-    console.log("1");
+    console.log("5");
     evt.preventDefault();
     if (blockInPlay.length !== 0 || leftB.childElementCount !== 0) {
-      console.log("2");
+      console.log("6");
       if (blockInPlay.length == 0) {
         blockInPlay.push(leftB.lastElementChild);
         leftB.removeChild(leftB.lastElementChild);
+        console.log("7");
       } else {
+        console.log("8");
         leftB.insertBefore(blockInPlay[0], leftB.lastchildElement);
         blockInPlay.pop();
       }
-      move += 1;
     }
   }
+  // if (blockInPlay.length == 1 && leftB.childElementCount == 0) {
+  //   console.log("5");
+  //   evt.preventDefault();
+  //   if (blockInPlay.length !== 0 || leftB.childElementCount !== 0) {
+  //     console.log("6");
+  //     if (blockInPlay.length == 0) {
+  //       blockInPlay.push(leftB.lastElementChild);
+  //       leftB.removeChild(leftB.lastElementChild);
+  //       console.log("7");
+  //     } else {
+  //       console.log("8");
+  //       leftB.insertBefore(blockInPlay[0], leftB.lastchildElement);
+  //       blockInPlay.pop();
+  //     }
+  //   }
+  // }
+  moves += 0.5;
+  h4.innerText = "MOVES " + moves;
 });
 
 mid.addEventListener("click", function(evt) {
   if (blockInPlay.length == 0 || mid.childElementCount == 0) {
-    console.log("1");
     evt.preventDefault();
     if (blockInPlay.length !== 0 || mid.childElementCount !== 0) {
-      console.log("2");
       if (blockInPlay.length == 0) {
         blockInPlay.push(mid.lastElementChild);
         mid.removeChild(mid.lastElementChild);
@@ -62,13 +100,10 @@ mid.addEventListener("click", function(evt) {
         mid.insertBefore(blockInPlay[0], mid.lastchildElement);
         blockInPlay.pop();
       }
-      move += 1;
     }
   } else if (blockInPlay[0].innerText > mid.lastElementChild.innerText) {
-    console.log("1");
     evt.preventDefault();
     if (blockInPlay.length !== 0 || mid.childElementCount !== 0) {
-      console.log("2");
       if (blockInPlay.length == 0) {
         blockInPlay.push(mid.lastElementChild);
         mid.removeChild(mid.lastElementChild);
@@ -76,17 +111,16 @@ mid.addEventListener("click", function(evt) {
         mid.insertBefore(blockInPlay[0], mid.lastchildElement);
         blockInPlay.pop();
       }
-      move += 1;
     }
   }
+  moves += 0.5;
+  h4.innerText = "MOVES " + moves;
 });
 
 rightB.addEventListener("click", function(evt) {
   if (blockInPlay.length == 0 || rightB.childElementCount == 0) {
-    console.log("1");
     evt.preventDefault();
     if (blockInPlay.length !== 0 || rightB.childElementCount !== 0) {
-      console.log("2");
       if (blockInPlay.length == 0) {
         blockInPlay.push(rightB.lastElementChild);
         rightB.removeChild(rightB.lastElementChild);
@@ -94,13 +128,10 @@ rightB.addEventListener("click", function(evt) {
         rightB.insertBefore(blockInPlay[0], rightB.lastchildElement);
         blockInPlay.pop();
       }
-      move += 1;
     }
   } else if (blockInPlay[0].innerText > rightB.lastElementChild.innerText) {
-    console.log("1");
     evt.preventDefault();
     if (blockInPlay.length !== 0 || rightB.childElementCount !== 0) {
-      console.log("2");
       if (blockInPlay.length == 0) {
         blockInPlay.push(rightB.lastElementChild);
         rightB.removeChild(rightB.lastElementChild);
@@ -108,14 +139,42 @@ rightB.addEventListener("click", function(evt) {
         rightB.insertBefore(blockInPlay[0], rightB.lastchildElement);
         blockInPlay.pop();
       }
-      move += 1;
     }
   }
-  if (rightB.childElementCount == 3) {
+
+  if (rightB.childElementCount == n) {
     finisher();
   }
+  moves += 0.5;
+  h4.innerText = "MOVES " + moves;
 });
-
+var secs = 0;
 function finisher() {
   alert("YOUR DONE");
+  finisher = true;
+
+  if (moves >= Math.pow(2, n)) {
+    alert("YOU CAN DO BETTER");
+  }
+}
+
+window.onload = function() {
+  //GOT THIS FROM A WEBSITE https://stackoverflow.com/questions/34278816/how-to-add-a-timer-in-html5-and-javascript
+  var id = setInterval(function() {
+    secs++;
+    h3.innerText = "TIMER " + secs;
+    if (finisher == true) {
+      clearInterval(id); //GOT THIS FROM A WEBSITE https://stackoverflow.com/questions/34278816/how-to-add-a-timer-in-html5-and-javascript
+      h3.innerText = "Total Time: " + secs + " seconds";
+    }
+  }, 1000);
+};
+function levelFour() {
+  bigBlock.style.display = "block";
+}
+function levelFive() {
+  biggerBlock.style.display = "block";
+}
+function levelSix() {
+  bigBodBlock.style.display = "block";
 }
